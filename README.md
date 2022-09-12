d1d8cb76d45e5ebe1b5620b6623f44eb2333851c
Janelin126 <79820235+Janelin126@users.noreply.github.com>
2022-09-12 15:56:03 +1200

    week 7 js

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # 212-arcade
- 
+
+## virtual environment -- mac
+python3 -m venv env
+source env/bin/activate
+pip install flask
+
+## virtual environment -- windows
+py -m venv env  
+env\Scripts\activate.bat
+pip install Flask
+
+## running flask --mac
+cd 212-arcade
+source env/bin/activate
+export FLASK_APP=run.py
+export FLASK_DEBUG=1
+flask run
---
+++ static/js.js
@@ -0,0 +1,41 @@
+// variables
+
+let colors = [
+  '#D00',
+  '#DD0',
+  '#0DD',
+  '#22D',
+  '#D0D'
+];
+
+let scores = [
+  ['TAB','1991-03-03','12345'],
+  ['BAT','1991-02-02','5000'],
+  ['JEM','1991-01-01','1000']
+];
+
+let topscore = 13000;
+
+document.getElementById('highscores').innerHTML = '<tr><th>[NAME]</th><th>[DATE]</th><th>[SCORE]</th></tr>';
+document.getElementById('chart').innerHTML = '';
+
+for (let i=0; i < scores.length; i++) {   
+  let color = colors[i%colors.length];
+
+  let bar = document.createElement('div');
+  bar.className = 'bar';
+  bar.style.width = scores[i][2] / topscore * 100 + '%';
+  bar.style.backgroundColor = color;
+  document.getElementById('chart').appendChild(bar);
+
+  let row = document.createElement('tr');
+  row.style.color = color;
+  row.innerHTML += '<td>' + scores[i][0] + '</td>';
+  row.innerHTML += '<td>' + scores[i][1] + '</td>';
+  row.innerHTML += '<td>' + scores[i][2] + '</td>';
+  document.getElementById('highscores').appendChild(row);
+}
+
+// insert entry
+
+let bar = document.createElement('div');
